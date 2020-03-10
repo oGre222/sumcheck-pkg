098ce38edd9c91d6f99ed25ab620877fcdc5216d
马骉 <mabiao1@staff.weibo.com>
2020-03-10 10:57:37 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/oGre222/sumcheck-pkg
+
+go 1.12
+
+require github.com/weibreeze/breeze-go v0.1.0 // indirect
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/weibreeze/breeze-go"
+)
+
+func main() {
+	var f float64
+	b := breeze.NewBuffer(32)
+	breeze.WriteStringType(b)
+	b.WriteByte(byte(30))
+
+	var data []byte
+	for i:=0; i< 30 ;i++  {
+		data = append(data, 'a')
+	}
+
+	b.Write(data)
+	bErr := breeze.ReadFloat64(b, &f)
+	fmt.Println(bErr, f)
+}
